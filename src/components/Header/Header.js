6da5ce28e472196aa3cd83cdadbe54c75aec188b
Viveros Bedoya Yazuline <Yazuline.Viveros@carvajal.com>
2023-08-23 14:27:03 -0500

    se agregan librerias y añado header con estilo scss

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss'
+import TwitterLogo from '../../Assets/img/twitter-logo.png'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <img src={TwitterLogo}  alt="Tweets Simulator" />
+        <h1> Tweets Simulator</h1>
+
+    </div>
+  )
+}
+
+export default Header
